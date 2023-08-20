d51d92bceefd83830d070199cc2d30a34e9ab788
siri <maroju.siri123@gmail.com>
2023-08-20 19:11:09 +0000

    first commit on child

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println("This is master class file from git");
+System.out.println("This is master class file from git change for child");
 }
 }
